868d86eaa1147b4102e8b1ea0e971c8f8daa2b26
dapin13148 <dapin13148@163.com>
2019-08-29 00:36:01 +0800

    第次+++++

--- src/test/java/package1/class1.java
+++ src/test/java/package1/class1.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class class1 {
 	@BeforeSuite
 	public void testBeforeSuite(){
-		System.out.println("testBeforeSuite-----------");
+		System.out.println("testBeforeSuite-----------++++++++");
 	}
 	
 	@Test(groups={"group1","group2"})
